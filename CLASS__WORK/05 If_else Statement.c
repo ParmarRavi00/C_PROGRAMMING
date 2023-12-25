454030876e71aedeb9c0351d0953639e1c255edd
Parmar Raviraj Rajendrasinh <parmarraviraj00@gmail.com>
2023-12-25 15:12:51 +0530

    .

--- CLASS__WORK/05 If_else Statement.c
+++ CLASS__WORK/05 If_else Statement.c
@@ -22,7 +22,7 @@
 		}
 
 
-3) else if Statement:-
+3) Ladder if else Statement:-
 
 		if(Condition)
 		{
@@ -37,8 +37,38 @@
 			Statement;
 		}
 
+4)switch statement.
 
-*/	
+	syntax: 
+		switch(condition)
+		{
+			case type:statement
+				  break;
+			case type:statement
+				  break;
+			case type:statement
+				  break;
+			.
+			.
+			.
+			default :statement
+				 break;
+		}	
+
+		
+
+
+Jumping keywords;
+
+1)break.
+2)continue.
+3)goto.
+
+Comments : 
+
+1) Single-line :   //
+
+2) Multi-Line  : /*  code  */
 	
 
 // Check if person can Vote or not
